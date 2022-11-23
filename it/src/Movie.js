d1d42b7075fc91f3e814dac63c77d9178ecbf06b
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-23 19:17:49 +0530

    react class access

---
+++ it/src/Movie.js
@@ -0,0 +1,16 @@
+let Movie=()=>{
+    let movie_name="avatar"
+    let Director="james cameran"
+    let information={
+        main_role:"nothing",
+        heroien:"don't know"
+
+    }
+    return<div>
+
+        <p>{movie_name}</p>
+        <p>Director : {Director}</p>
+        <p> main_role : {information.main_role}</p>
+    </div>
+}
+export default Movie
--- it/src/index.js
+++ it/src/index.js
@@ -1,2 +1,4 @@
 import reactDOM from 'react-dom'
-reactDOM.render("<h2> hello gm</h2>",document.getElementById("aa"))
+import Movie from './Movie'
+reactDOM.render(<h2> hello gm</h2>,document.getElementById("aa"))
+reactDOM.render(<Movie />,document.getElementById("aa"))
