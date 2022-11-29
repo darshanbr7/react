9cb542294a9436aa8cfa0473ca165faabb631c70
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-11-29 10:29:49 +0530

    binding

--- it/src/bind/Message.jsx
+++ it/src/bind/Message.jsx
@@ -12,7 +12,7 @@ class Message  extends React.Component{
     render(){
         return<>
         <p> Message:{this.state.msg}</p>
-        <button onClick={this.binding.bind(this,"Login succesfully")}> Login</button>&nbsp;
+        <button onClick={this.binding.bind(this,"Login succesfully")}> Login</button> &nbsp &nbsp;
         <button onClick={this.binding.bind(this,"logout Succesfully")}> Logout</button>
 
         </>
