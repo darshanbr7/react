7706d1cb2c9636fed6dd043191114b54aaf95ee9
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-08 10:56:01 +0530

    usestate in form

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,17 +1,31 @@
+// import React from 'react'
+// import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+// import Navbar from './Navbar/Navbar'
+// import Home from "./Navbar/Home"
+
+// const App = () => {
+//   return (
+//     <>
+//     <Router>
+//       <Navbar/>
+//       <Routes>
+//         <Route path='./Home' element={<Home/>}    />
+//       </Routes>
+//     </Router>
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import Navbar from './Navbar/Navbar'
-import Home from "./Navbar/Home"
+import Login from './Forms/Login'
 
 const App = () => {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='./Home' element={<Home/>}    />
-      </Routes>
-    </Router>
+    <Login />
     </>
   )
 }
---
+++ practice/src/Forms/Login-3.jsx
@@ -0,0 +1,51 @@
+// in this fporm we are updatting the value inside the evnet 
+import React from 'react'
+import { useState } from 'react'
+// import { json } from 'react-router-dom'
+
+const Login = () => {
+   let[form,stForm]=useState({email:"",password:""})
+ 
+   
+  return (
+    <><pre>{JSON.stringify(form)}</pre>
+    <div className="container">
+        <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-4">
+                <div className="card mt-5">
+                    <div className="card-headder bg-warning">
+                        <div className="row">
+                            <div className="col-2"></div>
+                            <div className="col">
+                             <h1>   Login Page </h1>  
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-body bg-info">
+                        <form  onSubmit={(event)=>{
+                            event.preventDefault()
+                        }}>
+                           < div  className='form-group'>
+                            <input type="text" name="email" onChange={(event)=>{
+                                stForm({...form,email:event.target.value})
+                            }} placeholder='Email' className='form-control'/>
+                           </div>
+                           <div className='form-group'>
+                            <input type="text" name="password"   onChange={(event)=>{
+                                stForm({...form,password:event.target.value})
+                            }} id="" placeholder='Password' className='Form-control' />
+                           </div>
+                           <button className='btn btn-warning  ml-5 ml-5'>Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Login
