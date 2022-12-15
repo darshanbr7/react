115f51307bebce38f985d5627953517fd00e50d1
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-15 23:58:21 +0530

    kk

--- apm/src/Component/Home.jsx
+++ apm/src/Component/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
    <React.Fragment>
     <nav className="navbar navbar-expand-lg bg-dark mt-3">
         <div className="navbar-brand text-white"> Apartment Management</div>
-        <div className="ml-auto">
+        <div className="ml-auto">  
             <ul className="navbar-nav">
                 <li className="nav-link"> <Link to="" className="nav-item text-white">Home</Link></li>
                 <li className="nav-link"> <Link to="./login" className="nav-item text-white">login</Link></li>
